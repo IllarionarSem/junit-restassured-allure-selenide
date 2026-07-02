8ad51a07e931af806735f794852d9178347e295c
= <=>
2026-07-02 14:13:36 +0400

    added chrome capabilities

--- core/src/main/java/driver/SelenideConfiguration.java
+++ core/src/main/java/driver/SelenideConfiguration.java
@@ -3,6 +3,7 @@ package driver;
 import com.codeborne.selenide.Configuration;
 import config.UiConfig;
 import lombok.experimental.UtilityClass;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 @UtilityClass
 public class SelenideConfiguration {
@@ -16,5 +17,14 @@ public class SelenideConfiguration {
         Configuration.screenshots = config.screenshots();
         Configuration.timeout = config.timeout();
         Configuration.headless = config.headless();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--window-size=1920,1080"
+        );
+
+        Configuration.browserCapabilities = options;
     }
 }
