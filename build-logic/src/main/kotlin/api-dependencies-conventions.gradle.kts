8857c408131a0466755d17228b496ddeb0660d1a
= <=>
2026-07-03 16:16:54 +0400

    clear reports

--- build-logic/src/main/kotlin/api-dependencies-conventions.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    `java-library`
-}
-
-var libs = extensions.getByType(VersionCatalogsExtension::class.java).named("libs")
-
-dependencies {
-    api(libs.findLibrary("restAssured").get())
-}
