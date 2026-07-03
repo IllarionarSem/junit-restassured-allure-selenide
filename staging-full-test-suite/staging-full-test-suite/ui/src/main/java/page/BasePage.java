0cca1ba7507fca4be9b0075c54b801ae76d861b4
= <=>
2026-07-03 14:34:06 +0400

    clear reports

--- staging-full-test-suite/staging-full-test-suite/ui/src/main/java/page/BasePage.java
+++
@@ -1,8 +0,0 @@
-package page;
-
-public abstract class BasePage<T extends BasePage<T>> {
-
-    public T navigate(T clazz) {
-        return clazz;
-    }
-}
--- staging-full-test-suite/test/src/test/java/com/toolshop/ui/BaseTest.java
+++
@@ -1,8 +0,0 @@
-package com.toolshop.ui;
-
-import junitextensions.SelenideExtension;
-import org.junit.jupiter.api.extension.ExtendWith;
-
-@ExtendWith(SelenideExtension.class)
-public class BaseTest {
-}
